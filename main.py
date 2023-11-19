f27b8dcc2d83419cf56fc253c626657f78d3fcd6
TazakiN <tazkia.nizami@gmail.com>
2023-11-19 21:27:33 +0700

    feat: main.py?

--- main.py
+++ main.py
@@ -1,100 +1,69 @@
 import sys
 
-start_input = ""  # input word to be found or not found
-found = 0  # stores found state
-accepted_config = []  # here we will post end configuration that was accepted
 
+class PDA:
+    def __init__(self, definition_file):
+        with open(definition_file, "r") as file:
+            lines = file.readlines()
 
-# production rules ("read input", "pop stack", "push stack", "next state")
-productions = {}
+        self.states = lines[0].strip().split()
+        self.input_symbols = lines[1].strip().split()
+        self.stack_symbols = lines[2].strip().split()
+        self.start_state = lines[3].strip()
+        self.start_stack = lines[4].strip()
+        self.accept_states = lines[5].strip().split()
+        self.accept_condition = lines[6].strip()
 
-# all states or non-terminals (not really necessary)
-states = []
+        self.transitions = {}
+        for line in lines[7:]:
+            (
+                current_state,
+                input_symbol,
+                stack_top,
+                next_state,
+                new_stack_top,
+            ) = line.strip().split()
+            self.transitions[(current_state, input_symbol, stack_top)] = (
+                next_state,
+                new_stack_top,
+            )
 
-# list of alphabet symbols or terminals (not really necessary)
-symbols = []
+    def check_input(self, input):
+        current_state = self.start_state
+        current_stack = [self.start_stack]
 
-# list of stack alphabet symbols (not really necessary)
-stack_symbols = []
+        for symbol in input:
+            if (current_state, symbol, current_stack[-1]) in self.transitions:
+                current_state, new_stack_top = self.transitions[
+                    (current_state, symbol, current_stack[-1])
+                ]
+                if new_stack_top != "e":
+                    current_stack.append(new_stack_top)
+            elif (current_state, "e", current_stack[-1]) in self.transitions:
+                current_state, new_stack_top = self.transitions[
+                    (current_state, "e", current_stack[-1])
+                ]
+                if new_stack_top != "e":
+                    current_stack.append(new_stack_top)
+            else:
+                return False
 
-# start state
-start_symbol = ""
-
-# start stack symbol
-stack_start = ""
-
-# list of acceptable states
-acceptable_states = []
-
-# E - accept on empty stack or F - acceptable state (default is false)
-accept_with = ""
-
-
-def parse_file(filename):
-    global productions
-    global start_symbol
-    global start_stack
-    global acceptable_states
-    global accept_with
-
-    try:
-        lines = [line.rstrip() for line in open(filename)]
-
-    except:
-        return 0
-
-    # add start state
-    start_symbol = lines[3]
-
-    # add start stack symbol
-    start_stack = lines[4]
-
-    # list of acceptable states
-    acceptable_states.extend(lines[5].split())
-
-    # E - accept on empty stack or F - acceptable state (default is false)
-    accept_with = lines[6]
-
-    # add rules
-    for i in range(7, len(lines)):
-        production = lines[i].split()
-
-        configuration = [(production[1], production[2], production[4], production[3])]
-
-        if not production[0] in productions.keys():
-            productions[production[0]] = []
-
-        configuration = [
-            tuple(s if s != "e" else "" for s in tup) for tup in configuration
-        ]
-
-        productions[production[0]].extend(configuration)
-
-    print(productions)
-    print(start_symbol)
-    print(start_stack)
-    print(acceptable_states)
-    print(accept_with)
-
-    return 1
+        if current_state in self.accept_states:
+            return True
+        else:
+            return False
 
 
 def main():
-    # Mengecek jumlah argumen yang diberikan
-    if len(sys.argv) != 3:
-        print("Cara penggunaan: python main.py <nama_file_pda> <nama_file_input>")
-        return
+    pda = PDA(sys.argv[1])
 
-    # Mengambil nama file dari argumen baris perintah
-    pda_file = sys.argv[1]
-    input_file = sys.argv[2]
+    with open(sys.argv[2], "r") as file:
+        html_code = file.read()
 
-    try:
-        parse_file(pda_file)
-        print("PDA berhasil dibaca.")
-
-    except FileNotFoundError:
-        print("File tidak ditemukan.")
+    if pda.check_input(html_code):
+        print("Accepted")
+    else:
+        print("Syntax Error")
 
 
 if __name__ == "__main__":
