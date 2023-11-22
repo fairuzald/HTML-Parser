0954b899c4a89ef278bb3e983b3b8978b6b2a425
TazakiN <tazkia.nizami@gmail.com>
2023-11-22 22:20:18 +0700

    feat: finalize pda.txt

--- parse.py
+++ parse.py
@@ -1,28 +1,32 @@
-# with open("pda.txt", "r") as file:
-#     lines = file.readlines()
-
-# with open("pdatest.txt", "w") as file:
-#     for line in lines:
-#         if (
-#             line.strip()
-#             and not line.startswith("#")
-#             and not line.startswith("A")
-#             and not line.startswith("E")
-#         ):
-#             file.write(line)
-
-with open("pdatest.txt", "r") as file:
+with open("pda pake komen.txt", "r") as file:
     lines = file.readlines()
 
-new_lines = []
-for line in lines:
-    words = line.split()
-    if len(words) >= 4 and words[3].startswith("A"):
-        words[3] = "F" + words[3][1:]
-    new_lines.append(" ".join(words))
+with open("pda.txt", "w") as file:
+    for line in lines:
+        if line.strip() and not line.startswith("#"):
+            file.write(line)
 
-with open("pdatest1.txt", "w") as file:
-    file.write("\n".join(new_lines))
+
+# with open("pda1.txt", "r") as file:
+#     lines = file.readlines()
+
+# for i, line in enumerate(lines[7:], start=7):
+#     parts = line.strip().split()
+#     if len(parts) != 5:
+#         print(f"Line {i+1} does not have 5 values after being stripped and split.")
+
+# with open("pdatest.txt", "r") as file:
+#     lines = file.readlines()
+
+# new_lines = []
+# for line in lines:
+#     words = line.split()
+#     if len(words) >= 4 and words[3].startswith("A"):
+#         words[3] = "F" + words[3][1:]
+#     new_lines.append(" ".join(words))
+
+# with open("pdatest1.txt", "w") as file:
+#     file.write("\n".join(new_lines))
 
 ### PRINT ALL STATES ###
 # states = set()
@@ -53,7 +57,7 @@ with open("pdatest1.txt", "w") as file:
 
 ### PRINT ALL STACK ###
 # states = set()
-# with open("pda pake komen.txt", "r") as file:
+# with open("pda pake .txt", "r") as file:
 #     for line in file:
 #         parts = line.split()
 #         if line.startswith("#"):
