fcbba88c3b8c72130eee6c9144a49a2030e74570
TazakiN <tazkia.nizami@gmail.com>
2023-11-25 17:30:23 +0700

    feat: handle comments (not tested)

--- parse.py
+++ parse.py
@@ -1,10 +1,10 @@
-with open("pda pake komen2.txt", "r") as file:
-    lines = file.readlines()
+# with open("pda pake komen2.txt", "r") as file:
+#     lines = file.readlines()
 
-with open("pda2.txt", "w") as file:
-    for line in lines:
-        if line.strip() and not line.startswith("#"):
-            file.write(line)
+# with open("pda2.txt", "w") as file:
+#     for line in lines:
+#         if line.strip() and not line.startswith("#"):
+#             file.write(line)
 
 
 # with open("pda1.txt", "r") as file:
@@ -29,22 +29,22 @@ with open("pda2.txt", "w") as file:
 #     file.write("\n".join(new_lines))
 
 ### PRINT ALL STATES ###
-# states = set()
-# with open("pda2.txt", "r") as file:
-#     for line in file:
-#         if line.startswith("#"):
-#             continue
-#         parts = line.split()
-#         if len(parts) >= 4:
-#             states.add(parts[0])
-#             states.add(parts[3])
+states = set()
+with open("pda pake komen2.txt", "r") as file:
+    for line in file:
+        if line.startswith("#"):
+            continue
+        parts = line.split()
+        if len(parts) >= 4:
+            states.add(parts[0])
+            states.add(parts[3])
 
-# states_str = " ".join(states)
-# print(states_str)
+states_str = " ".join(states)
+print(states_str)
 
 ### PRINT ALL INPUT ###
 # states = set()
-# with open("pda pake komen.txt", "r") as file:
+# with open("pda pake komen2.txt", "r") as file:
 #     for line in file:
 #         parts = line.split()
 #         if line.startswith("#"):
@@ -57,7 +57,7 @@ with open("pda2.txt", "w") as file:
 
 ### PRINT ALL STACK ###
 # states = set()
-# with open("pda.txt", "r") as file:
+# with open("pda pake komen2.txt", "r") as file:
 #     for line in file:
 #         parts = line.split()
 #         if line.startswith("#"):
